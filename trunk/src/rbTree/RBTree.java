10b6cb5cd6c68a0f50cfa5f030c854269e04828f
kaio.ufcg@gmail.com <kaio.ufcg@gmail.com@07370e68-ee4a-1641-a0f9-039d6d31befa>
2011-05-30 17:26:39 +0000

    ULTIMA VERSAO

--- trunk/src/rbTree/RBTree.java
+++ trunk/src/rbTree/RBTree.java
@@ -1,116 +1,120 @@
 package rbTree;
 
-import rbTree.RBTreeImpl;
 import util.ADTOverflowException;
 import util.ADTUnderflowException;
 
 /**
- * Um nó de uma RBTree. Deve ter campos para manter uma chave de tipo K
- * (comparável) e um valor de tipo V (também comparáveis). Ele também tem um
- * pai, um filho esquerdo e um direito da criança e uma bandeira para determinar
- * se o nó é preto ou não (se ele não é negro, então é assumido que o nó é
+ * Um nÃ³ de uma RBTree. Deve ter campos para manter uma chave de tipo K
+ * (comparÃ¡vel) e um valor de tipo V (tambÃ©m comparÃ¡veis). Ele tambÃ©m tem um
+ * pai, um filho esquerdo e um direito da crianÃ§a e uma bandeira para determinar
+ * se o nÃ³ Ã© preto ou nÃ£o (se ele nÃ£o Ã© negro, entÃ£o Ã© assumido que o nÃ³ Ã©
  * vermelho).
  */
 class RBNode<K extends Comparable<K>, V extends Comparable<V>> {
 
-	private K chave;
-	private V dadoSatelite;
-	private RBTreeImpl<K, V> filhoEsquerdo;
-	private RBTreeImpl<K, V> filhoDireito;
-	private RBTreeImpl<K, V> pai;
-	private PVCores cor = PVCores.PRETO;
+    private RBNode<K, V> pai;
+    private RBNode<K, V> filhoEsquerda;
+    private RBNode<K, V> filhoDireita;
+    private K chave;
+    private V valor;
+    private boolean isBlack = true;
 
-	/**
-	 * Metodo para verificar se o no eh vazio ou nao
-	 * 
-	 * @return true ou false, dependendo se o filho esquerdo e direito sao
-	 *         nulos.
-	 */
-	public boolean isEmpty() {
-		return this.getFilhoEsquerdo() == null && this.getFilhoEsquerdo() == null;
-	}
+    /**
+     * Metodo para verificar se o no eh vazio ou nao
+     * 
+     * @return true ou false, dependendo se o filho esquerdo e direito sao
+     *         nulos.
+     */
+    public boolean isEmpty() {
+        return this.getFilhoEsquerda() == null && this.getFilhoEsquerda() == null;
+    }
 
-	/**
-	 * Metodo para verificar se o no eh folha ou nao
-	 * 
-	 * @return true ou false, dependendo se o filho esquerdo e direito sao
-	 *         vazios.
-	 */
-	public boolean isLeaf() {
-		boolean resp = false;
-		if (!isEmpty()) {
-			resp = this.getFilhoEsquerdo().isEmpty() && this.getFilhoDireito().isEmpty();
-		}
-		return resp;
-	}
+    /**
+     * Metodo para verificar se o no eh folha ou nao
+     * 
+     * @return true ou false, dependendo se o filho esquerdo e direito sao
+     *         vazios.
+     */
+    public boolean isLeaf() {
+        boolean resp = false;
+        if (!isEmpty()) {
+            resp = this.getFilhoEsquerda().isEmpty() && this.getFilhoDireita().isEmpty();
+        }
+        return resp;
+    }
 
-	/**
-	 * Este método deve retornar uma string contendo a chave de um nó de RB. Se
-	 * o nó é preto ele simplesmente retorna a chave (como String). Caso
-	 * contrário, ele retorna a chave entre menor e maior (ou seja, <key>).
-	 */
-	@Override
-	public String toString() {
-		StringBuilder resp = new StringBuilder();
-		if (!isEmpty()) {
-			if (this.getCor().equals(PVCores.PRETO)) {
-				resp.append("<" + this.getDadoSatelite() + "> ");
-			} else {
-				resp.append(this.getDadoSatelite() + " ");
-			}
-			resp.append(this.getFilhoEsquerdo().toString());
-			resp.append(this.getFilhoDireito().toString());
-		}
-		return resp.toString();
-	}
+    /**
+     * Este mÃ©todo deve retornar uma string contendo a chave de um nÃ³ de RB. Se
+     * o nÃ³ Ã© preto ele simplesmente retorna a chave (como String). Caso
+     * contrÃ¡rio, ele retorna a chave entre menor e maior (ou seja, <key>).
+     */
+    @Override
+    public String toString() {
+        StringBuilder resp = new StringBuilder();
+        if (!isEmpty()) {
+            if (this.getCor()) {
+                resp.append(this.getValor() + " ");
+            } else {
+                resp.append("<" + this.getValor() + "> ");
 
-	public K getChave() {
-		return chave;
-	}
+            }
+            resp.append(this.getFilhoEsquerda().toString());
+            resp.append(this.getFilhoDireita().toString());
+        }
+        return resp.toString();
+    }
 
-	public PVCores getCor() {
-		return cor;
-	}
+    public RBNode<K, V> getPai() {
+        return this.pai;
+    }
 
-	public V getDadoSatelite() {
-		return dadoSatelite;
-	}
+    public RBNode<K, V> getFilhoEsquerda() {
+        return this.filhoEsquerda;
+    }
 
-	public RBTreeImpl<K, V> getFilhoDireito() {
-		return filhoDireito;
-	}
+    public RBNode<K, V> getFilhoDireita() {
+        return this.filhoDireita;
+    }
 
-	public RBTreeImpl<K, V> getFilhoEsquerdo() {
-		return filhoEsquerdo;
-	}
+    public K getChave() {
+        return this.chave;
+    }
 
-	public RBTreeImpl<K, V> getPai() {
-		return pai;
-	}
+    public V getValor() {
+        return this.valor;
+    }
 
-	public void setChave(K chave) {
-		this.chave = chave;
-	}
+    public boolean getCor() {
+        return this.isBlack;
+    }
 
-	public void setCor(PVCores cor) {
-		this.cor = cor;
-	}
+    public void setPai(RBNode<K, V> pai) {
+        this.pai = pai;
+    }
 
-	public void setDadoSatelite(V dadoSatelite) {
-		this.dadoSatelite = dadoSatelite;
-	}
+    public void setFilhoEsquerda(RBNode<K, V> filhoEsquerda) {
+        this.filhoEsquerda = filhoEsquerda;
+    }
 
-	public void setFilhoDireito(RBTreeImpl<K, V> filhoDireito) {
-		this.filhoDireito = filhoDireito;
-	}
+    public void setFilhoDireita(RBNode<K, V> filhoDireita) {
+        this.filhoDireita = filhoDireita;
+    }
 
-	public void setFilhoEsquerdo(RBTreeImpl<K, V> filhoEsquerdo) {
-		this.filhoEsquerdo = filhoEsquerdo;
-	}
+    public void setChave(K chave) {
+        this.chave = chave;
+    }
 
-	public void setPai(RBTreeImpl<K, V> pai) {
-		this.pai = pai;
-	}
+    public void setValor(V valor) {
+        this.valor = valor;
+    }
+
+    public void setCor() {
+        if (isBlack) {
+            this.isBlack = false;
+        } else {
+            this.isBlack = true;
+        }
+    }
 }
 
 /**
@@ -122,88 +126,87 @@ class RBNode<K extends Comparable<K>, V extends Comparable<V>> {
  */
 public interface RBTree<K extends Comparable<K>, V extends Comparable<V>> {
 
-	/**
-	 * Retorna a altura de uma árvore RB. NIL nós não influenciam os altura.
-	 */
-	public int height();
+    /**
+     * Retorna a altura de uma Ã¡rvore RB. NIL nÃ³s nÃ£o influenciam os altura.
+     */
+    public int height();
 
-	/**
-	 * Retorna a altura de uma árvore negra RB.
-	 */
-	public int blackHeight();
+    /**
+     * Retorna a altura de uma Ã¡rvore negra RB.
+     */
+    public int blackHeight();
 
-	/**
-	 * Procura o valor associado a uma determinada chave. Retorna nulo se a
-	 * chave é não na árvore.
-	 */
-	public V search(K key);
+    /**
+     * Procura o valor associado a uma determinada chave. Retorna nulo se a
+     * chave Ã© nÃ£o na Ã¡rvore.
+     */
+    public V search(K key);
 
-	/**
-	 * Insere um valor associado a uma determinada chave em uma árvore RB.
-	 */
-	public void insert(K key, V value) throws ADTOverflowException;
+    /**
+     * Insere um valor associado a uma determinada chave em uma Ã¡rvore RB.
+     */
+    public void insert(K key, V value) throws ADTOverflowException;
 
-	/**
-	 * Returns the maximum value (associated to the greatest key) of a RB tree.
-	 */
-	public V maximum();
+    /**
+     * Returns the maximum value (associated to the greatest key) of a RB tree.
+     */
+    public V maximum();
 
-	/**
-	 * Retorna o valor máximo (associada à maior chave) de uma árvore RB.
-	 */
-	public V minimum();
+    /**
+     * Retorna o valor mÃ¡ximo (associada Ã  maior chave) de uma Ã¡rvore RB.
+     */
+    public V minimum();
 
-	/**
-	 * Retorna a chave máxima de uma árvore ou nulo, se a árvore está vazia.
-	 */
-	public K maximumKey();
+    /**
+     * Retorna a chave mÃ¡xima de uma Ã¡rvore ou nulo, se a Ã¡rvore estÃ¡ vazia.
+     */
+    public K maximumKey();
 
-	/**
-	 * Retorna a chave mínima de uma árvore ou nulo, se a árvore está vazia.
-	 */
-	public K minimumKey();
+    /**
+     * Retorna a chave mÃ­nima de uma Ã¡rvore ou nulo, se a Ã¡rvore estÃ¡ vazia.
+     */
+    public K minimumKey();
 
-	/**
-	 * Returns the node that is the the successor (according to the Cormen's
-	 * book) of a node containing the given key. If the key is not in the tree
-	 * returns null;
-	 */
-	public RBNode<K, V> successor(K key);
+    /**
+     * Returns the node that is the the successor (according to the Cormen's
+     * book) of a node containing the given key. If the key is not in the tree
+     * returns null;
+     */
+    public RBNode<K, V> successor(K key);
 
-	/**
-	 * Retorna o nó que é o sucessor (de acordo com o Cormen está livro) de um
-	 * nó que contém a chave dada. Se a chave não está na árvore retorna null;
-	 */
-	public RBNode<K, V> predecessor(K key);
+    /**
+     * Retorna o nÃ³ que Ã© o sucessor (de acordo com o Cormen estÃ¡ livro) de um
+     * nÃ³ que contÃ©m a chave dada. Se a chave nÃ£o estÃ¡ na Ã¡rvore retorna null;
+     */
+    public RBNode<K, V> predecessor(K key);
 
-	/**
-	 * Remove o nó que contém uma determinada chave.
-	 * 
-	 * @param key
-	 */
-	public void delete(K key) throws ADTUnderflowException;
+    /**
+     * Remove o nÃ³ que contÃ©m uma determinada chave.
+     * 
+     * @param key
+     */
+    public void delete(K key) throws ADTUnderflowException;
 
-	/**
-	 * Retorna quantos elementos foram colocados na árvore.
-	 */
-	public int size();
+    /**
+     * Retorna quantos elementos foram colocados na Ã¡rvore.
+     */
+    public int size();
 
-	/**
-	 * percorre a árvore em ordem e retorna um array contendo os valores nessa
-	 * ordem.
-	 */
-	public V[] preOrder();
+    /**
+     * percorre a Ã¡rvore em ordem e retorna um array contendo os valores nessa
+     * ordem.
+     */
+    public V[] preOrder();
 
-	/**
-	 * percorre a árvore em ordem e retorna um array contendo os valores nessa
-	 * ordem.
-	 */
-	public V[] order();
+    /**
+     * percorre a Ã¡rvore em ordem e retorna um array contendo os valores nessa
+     * ordem.
+     */
+    public V[] order();
 
-	/**
-	 * percorre a árvore em pós-ordem e retorna um array contendo os Os valores,
-	 * nessa ordem.
-	 */
-	public V[] postOrder();
-
+    /**
+     * percorre a Ã¡rvore em pÃ³s-ordem e retorna um array contendo os Os valores,
+     * nessa ordem.
+     */
+    public V[] postOrder();
 }
